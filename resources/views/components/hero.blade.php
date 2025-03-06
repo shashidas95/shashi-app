dec93de3fd59f1e7005ba7c705d81d3714bcdc66
shashidas95 <shashidas95@gmail.com>
2025-03-06 20:11:27 +0600

    navbar hero updated

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,14 +2,14 @@
     <div class="container px-5 pb-5">
         <div class="row gx-5 align-items-center">
             <div class="col-xxl-5">
-                <!-- Header text content-->
+                <!-- Header text content      -->
                 <div class="text-center text-xxl-start">
                     <div class="badge bg-gradient-primary-to-secondary text-white mb-4">
-                        <div id="keyLine" class="text-uppercase">Design &middot; Development &middot; Marketing</div>
+                        <div id="keyLine" class="text-uppercase">AUTOMATE &middot; SCALE &middot; SECURE</div>
                     </div>
-                    <div id="title" class="fs-3 fw-light text-muted">I can help your business to</div>
-                    <h1 id="short_title" class="display-3 fw-bolder mb-5 text-gradient d-inline">Get online and grow
-                        fast</h1>
+                    <div id="title" class="fs-3 fw-light text-muted">I can help your infrastructure to</div>
+                    <h1 id="short_title" class="display-3 fw-bolder mb-5 text-gradient d-inline">Deploy faster and run
+                        smoother</h1>
                     <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                         <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder"
                             href="{{ url('/resume') }}">Resume</a>
